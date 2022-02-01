a3c0c5197f9982ba3e4aa0b5557b5fda8dc70286
Phabel Antonio López Delgado <phabel2001@gmail.com>
2022-02-01 17:59:12 -0600

    Actualizar 01-notas

--- R/01-notas.R
+++ R/01-notas.R
@@ -1,1 +1,22 @@
 # Archivo para notas generales de clase.
+# Base: https://lcolladotor.github.io/rnaseq_LCG-UNAM_2022/
+
+# 1) Bioconductor: http://bioconductor.org/
+
+Bioconductor es un repositorio de paquetes de R enfocado en el análisis
+y manejo de HT genomic data.
+
+Los tipos de paquetes encontrado en Bioc son:
+  *Software
+  *Annotation
+  *Experiment Data
+  *Workflows
+
+La documentación de los paquetes (vignette) es de suma importancia, dice mucho sobre
+la fiabilidad del mismo. Este usualmente se encuentra en un HTML o PDF en
+la página del paquete.
+
+Existen dos ramas de Bioc: release & devel. Pero es más seguro usar release pues
+ha sido más experimentada. Mientras que devel, como el nombre sugiere, es una rama
+en desarrollo dirigida por las personas que desarrollan la paquetería; suele ser más
+actual pero prona a errores e incompatibilidad.
