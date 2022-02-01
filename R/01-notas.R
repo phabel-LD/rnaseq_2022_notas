64e13529c1ee09dceab832f85ac3f43f9572530d
Phabel Antonio López Delgado <phabel2001@gmail.com>
2022-02-01 13:45:17 -0600

    Actividad de Bioconducto con el link al análisis principal.

---
+++ R/01-notas.R
@@ -0,0 +1,11 @@
+# Nombre del paquete: Disease Ontology Semantic and Enrichment analysis
+
+# Summary: This package implements five methods proposed by Resnik, Schlicker, Jiang, Lin and Wang respectively for measuring semantic similarities among DO terms and gene products. Enrichment analyses including hypergeometric model and gene set enrichment analysis are also implemented for discovering disease associations of high-throughput biological data.
+
+# Autor: Guangchuang Yu [aut, cre], Li-Gen Wang [ctb], Vladislav Petyuk
+
+# Errores Documentacion: Ha presentado errores el último fue reportado el 2 de diciembre del año pasado
+
+# Funcion en sistemas: Funciona con tres SO
+
+# Link: https://docs.google.com/document/d/170TqhGVr0bawSuM2e0usv9k9iE2L-cS6ER-lSB1ZJb8/edit?usp=sharing
