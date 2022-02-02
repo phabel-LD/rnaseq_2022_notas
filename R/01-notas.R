b642c27aa7c24c09bbd73e23e2c0b9354f4b79b3
Phabel Antonio López Delgado <phabel2001@gmail.com>
2022-02-02 09:59:46 -0600

    Actualizar notas y crear SummarizedExperiment

--- R/01-notas.R
+++ R/01-notas.R
@@ -20,3 +20,24 @@ Existen dos ramas de Bioc: release & devel. Pero es más seguro usar release pue
 ha sido más experimentada. Mientras que devel, como el nombre sugiere, es una rama
 en desarrollo dirigida por las personas que desarrollan la paquetería; suele ser más
 actual pero prona a errores e incompatibilidad.
+
+# 2) Objetos en Bioconductor
+
+* SummarizedExperiment (software): Almacena data de experimentos. Condensación
+de más de una tabla por objeto interrelacionadas.
+  -> rowRanges: Cada renglon es un gen.(m) Deriva del objeto DRanges.
+  -> colData: Cada columna es una muestra. (n) Cada columna es un atributo de muestra.
+  -> assay: (m x n) Puede haber más de una tabla con las mismas dims.
+  -> exptData: metadata libre extra accesable que no está incluida en assays.
+
+Se puede accesar a subconjuntos de tablas relacionadas.
+
+* GenomicRanges [GRanges] (software): Analiza HT seq data, para almacenar data sobre
+alineamientos e intervalos genómicos. Se pueden hacer comparaciones con seq referencia.
+Indica los intervalos cromosómicos. IRanges(Interval Ranges), Rle (Re Length Ext). Partes:
+  -> seqname
+  -> ranges
+  -> strand
+Se pueden crear nuevas variables y accesarles con rowData(se)
+
+* rtracklayer: interactua con buscadores genómicos y manipular su anotación para generar data.
---
+++ R/04-SummarizedExperiment.R
@@ -0,0 +1,62 @@
+## Lets build our first SummarizedExperiment object
+library("SummarizedExperiment")
+## ?SummarizedExperiment
+
+## De los ejemplos en la ayuda oficial
+
+## Creamos los datos para nuestro objeto de tipo SummarizedExperiment
+## para 200 genes a lo largo de 6 muestras
+nrows <- 200
+ncols <- 6
+## Números al azar de cuentas
+set.seed(20210223)
+counts <- matrix(runif(nrows * ncols, 1, 1e4), nrows)
+## Información de nuestros genes
+rowRanges <- GRanges(
+  rep(c("chr1", "chr2"), c(50, 150)),
+  IRanges(floor(runif(200, 1e5, 1e6)), width = 100), # Asumir longitud igual 100 bp p/c gen
+  strand = sample(c("+", "-"), 200, TRUE),
+  feature_id = sprintf("ID%03d", 1:200)
+)
+names(rowRanges) <- paste0("gene_", seq_len(length(rowRanges)))
+## Información de nuestras muestras
+colData <- DataFrame(
+  Treatment = rep(c("ChIP", "Input"), 3),
+  row.names = LETTERS[1:6]
+)
+## Juntamos ahora toda la información en un solo objeto de R
+rse <- SummarizedExperiment(
+  assays = SimpleList(counts = counts),
+  rowRanges = rowRanges,
+  colData = colData
+)
+
+## Exploremos el objeto resultante
+rse
+
+## Número de genes y muestras
+dim(rse)
+
+## IDs de nuestros genes y muestras
+dimnames(rse)
+
+## Nombres de tablas de cuentas que tenemos (RPKM, CPM, counts, logcounts, etc)
+assayNames(rse)
+
+## El inicio de nuestra tabla de cuentas
+head(assay(rse))
+
+## Información de los genes en un objeto de Bioconductor
+rowRanges(rse)
+
+## Tabla con información de los genes
+rowData(rse) # es idéntico a 'mcols(rowRanges(rseataFrame != data.framecolData(rse)))'
+
+## Tabla con información de las muestras. D
+## Ejercicio
+
+## Comando 1. Itera sobre genes - rows.
+rse[1:2, ]
+
+## Comando 2. Itera sobre samples - cols.
+rse[, c("A", "D", "F")]
