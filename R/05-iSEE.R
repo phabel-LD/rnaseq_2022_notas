8373afebd9351d7686278c5e154b7c4862c8dded
Phabel Antonio López Delgado <phabel2001@gmail.com>
2022-02-02 19:27:18 -0600

    Agregar directorio del heatmap

--- R/05-iSEE.R
+++ R/05-iSEE.R
@@ -17,8 +17,8 @@ iSEE::iSEE(sce_layer)
 
 ## Ejercicio 1: Reproducir la imagen deseada con iSEE y analizar MOBP, MBP & PCP4.
 ## El primer resultado se encuentra en Imagenes/1_ReducedDimensionalPlot1.pdf.
-## El segundo resultado se halla en ...
-## ... donde se puede apreciar que PCP4 y SNAP25 comparten un similaridad espacial
-# en su expresión, posiblemente porque ambos se involucran en procesos afines en la
-# sinaptogénesis. Mientras que MOBP y MBP poseen mayor expresión según la heatbar
-# (tendiendo a amarillo) en el nivel WM correspondiente a la White Matter del cerebro.
+## El segundo resultado se halla en Imagenes/2_ComplexHetmapPlot1.pdf
+## donde se puede apreciar que PCP4 y SNAP25 comparten un similaridad espacial
+## en su expresión, posiblemente porque ambos se involucran en procesos afines en la
+## sinaptogénesis. Mientras que MOBP y MBP poseen mayor expresión según la heatbar
+## (tendiendo a amarillo) en el nivel WM correspondiente a la White Matter del cerebro.
